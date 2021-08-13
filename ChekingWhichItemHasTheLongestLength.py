d257951d686ecca224aabba7e3ae1ee698571722
sarvin nami <78687226+srvn-nm@users.noreply.github.com>
2021-08-13 16:09:06 +0430

    Add files via upload

---
+++ ChekingWhichItemHasTheLongestLength.py
@@ -0,0 +1,8 @@
+# author : Sarvin Nami
+list1 = [ 1 , 2 , 44 , 22222 , 181299]
+n = 0
+for i in list1 :
+    while n < len(str(i)) :
+        n = len(str(i))
+        theLongest = i
+print(theLongest) 
